c977df3e3ae3b265b4d15f013d961a9dd2825cb6
MrLinZHui <742847492@qq.com>
2019-07-12 17:21:56 +0800

    pass FilterTest

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -2,8 +2,8 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +14,33 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+//        List<Integer>  array1 = new ArrayList<>();
+//        for(int i=0;i<this.array.size();i++){
+//            int num = array.get(i);
+//            if(num%2==0){
+//                array1.add(num);
+//            }
+//        }
+//        return array1;
+        return this.array.stream().filter(num -> num % 2==0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return this.array.stream().filter(num -> num % 3 == 0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        //    return this.array.stream().distinct().collect(Collectors.toList());
+//        return Arrays.stream(this.array.stream().collect(Collectors.toSet()).toArray()).c;
+//        List<Integer> list = new ArrayList<>();
+//        this.array.stream().collect(Collectors.toSet()).forEach(num -> list.add(num) );
+//        return  list;
+        return firstList.stream().filter(num->secondList.contains(num)).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return new ArrayList<>(array.stream().collect(Collectors.toSet()));
     }
 }
--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,9 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,11 +19,13 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return this.array.stream().map(num -> num*3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+       // throw new NotImplementedException();
+        return this.array.stream().map(num -> ((char)(num+96))+"").collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
@@ -29,7 +33,12 @@ public class MyMap {
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        //return this.array.stream().
+        this.array.sort((o1,o2) ->{
+            return o2 -o1;
+        });
+        return array;
     }
 
     public List<Integer> sortFromSmall() {
