719b173005e0a6e2e4c0327ee1a9cdc2fa700954
MrLinZHui <742847492@qq.com>
2019-07-13 22:37:43 +0800

    should_get_sum_of_evens

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,11 +2,27 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        ArrayList<Integer>arrayList = new ArrayList<Integer>();
+        if(leftBorder <=rightBorder){
+            while(leftBorder <=rightBorder){
+                arrayList.add(leftBorder++);
+            }
+        }else{
+            while(leftBorder >=rightBorder){
+                arrayList.add(rightBorder++);
+            }
+        }
+
+        int sum =arrayList.stream().filter(num->num%2==0).reduce(( num,num1) ->num+=num1).get().intValue();
+        return sum;
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
