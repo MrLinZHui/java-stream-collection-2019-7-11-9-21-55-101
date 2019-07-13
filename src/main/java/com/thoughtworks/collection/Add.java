80006e8e0f9f0449ad06de623d834ca03532cade
MrLinZHui <742847492@qq.com>
2019-07-13 23:14:51 +0800

    should_get_triple_and_add_two

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -5,6 +5,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Add {
@@ -30,11 +31,12 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().map(num ->3*num+2).reduce((num1,num2) ->num1 +=num2).get().intValue();
+        //throw new NotImplementedException();
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+       return arrayList.stream().map(num ->num%2==0?num:(num*3+2)).collect(Collectors.toList());
     }
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
